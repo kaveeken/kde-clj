03f89b57c70ab0013bc592abd36223ab458af199
kris <k.a.veeken@student.rug.nl>
2022-01-17 15:22:50 +0100

    made `x - xi` version of factory the default and got rid of `u`-vers

--- src/kde_clj/density.clj
+++ src/kde_clj/density.clj
@@ -2,24 +2,24 @@
 
 (load "utility")
 
+;;(defn kernel-density-factory-u
+;;  [kernel data bandwidth]
+;;  (let [n (count data)]
+;;    (fn [x] (/ (apply + (map #(kernel (/ (- x %1) bandwidth)) data))
+;;               (* n bandwidth)))))
+
 (defn kernel-density-factory
   [kernel data bandwidth]
   (let [n (count data)]
-    (fn [x] (/ (apply + (map #(kernel (/ (- x %1) bandwidth)) data))
-               (* n bandwidth)))))
-
-(defn kernel-density-deriv-factory
-  [kernel-deriv data bandwitdth]
-  (let [n (count data)]
-    (fn [x] (/ (apply + (map #(kernel-deriv (/ x bandwitdth) (/ %1 bandwitdth))
+    (fn [x] (/ (apply + (map #(kernel (/ x bandwidth) (/ %1 bandwidth))
                              data))
-               (* n bandwitdth)))))
+               (* n bandwidth)))))
 
 (defn compute-density
   ([kernel data]
    (compute-density kernel data (pick-bandwidth data)))
   ([kernel data bandwidth]
-   (let [kde (kernel-density-deriv-factory kernel data bandwidth)
+   (let [kde (kernel-density-factory kernel data bandwidth)
          x (pick-space data)]
    (map kde x))))
 
@@ -27,7 +27,16 @@
   ([kernel data]
    (build-density-plot kernel data (pick-bandwidth data)))
   ([kernel data bandwidth]
-   (let [kde (kernel-density-deriv-factory kernel data bandwidth)
+   (let [kde (kernel-density-factory kernel data bandwidth)
          x (pick-space data)
          density (map kde x)]
      (c/scatter-plot x density))))
+
+(defn integrate-analytical
+  ([kernel data a b]
+   (integrate-kde kernel data a b (pick-bandwidth data)))
+  ([kernel data a b bandwidth]
+   (let [kde-integral (kernel-density-factory kernel data bandwidth)]
+     (- (kde-integral b)
+        (kde-integral a)))))
+
